ae1f4407e98e39ee95dba61e59fa313c082a8a22
Alexander <alexanderholowinski@icloud.com>
2026-01-09 22:20:25 +1100

    Initial Commit

---
+++ SpinFlipRoll/SpinFlipRollApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpinFlipRollApp.swift
+//  SpinFlipRoll
+//
+//  Created by Alexander Holowinski on 9/1/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpinFlipRollApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
